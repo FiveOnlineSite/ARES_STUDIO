41e32ad1f5c8d645b2c41fc0af2e744c7b716cdd
AdarshFiveonline <helpdesk@fiveonline.in>
2024-09-24 11:16:13 +0530

    changed backend package.json

--- frontend/src/components/Lightboxcomponent.js
+++ frontend/src/components/Lightboxcomponent.js
@@ -118,7 +118,7 @@ const Lightboxcomponent = () => {
                                 "\\",
                                 "/"
                               )}`
-                            : undefined
+                            : "/images/about-bg.jpg"
                         }
                       />
                     ) : (
